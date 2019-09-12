62eb86b5eaa59008b512e595c215187683118f31
Administrator <Administrator@PMA4F33z745244-2.local>
2019-09-12 18:16:50 +0800

    initial

---
+++ Jenkins-Sample/Jenkins-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jenkins-Sample
+//
+//  Created by Administrator on 9/12/19.
+//  Copyright © 2019 Administrator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Jenkins-Sample/Jenkins-SampleTests/Jenkins_SampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  Jenkins_SampleTests.swift
+//  Jenkins-SampleTests
+//
+//  Created by Administrator on 9/12/19.
+//  Copyright © 2019 Administrator. All rights reserved.
+//
+
+import XCTest
+@testable import Jenkins_Sample
+
+class Jenkins_SampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
