dcc293d422a4d3acdcbb493cfdf0c6511e761229
Administrator <>
2019-09-16 19:45:23 +0800

    testing 2

--- Jenkins-Sample/Jenkins-Sample/ViewController.swift
+++ Jenkins-Sample/Jenkins-Sample/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("this is fr-001")
+        print("this is fr-001 updated")
     }
 
 
